ebce03af1d9725e70256ce4328d774718c2664a2
Reginald Amedee <reginald.amedee@gmail.com>
2022-05-26 12:31:09 -0400

    used redux to manage state

---
+++ index.js
@@ -0,0 +1,22 @@
+import configureStore from "redux";
+
+const initialState = [1,2,3]
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'add' : {
+            return [
+                ...state,
+                state.push(action.payload)
+            ]
+            
+        }
+
+        default:
+            return state
+    }
+}
+
+const add = reducer(initialState, {type: 'add', payload: 4})
+
+console.log(add)
